9d629ce0c7c8d40fcfc9f1133fe97245789840e1
Ashton George <ashtongeorge17@gmail.com>
2025-07-14 21:22:23 -0400

    additional styling and fixes for project page

--- src/app/projects/[project]/page.js
+++ src/app/projects/[project]/page.js
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import { client } from '@/sanity/lib/client';
 import { PROJECT_QUERY } from "@/sanity/lib/queries";
 import { PortableText } from '@portabletext/react';
-import Link from 'next/link';
 import gh from '../../../public/gh.png';
 
 export default async function Project({ params }) {
@@ -12,20 +11,22 @@ export default async function Project({ params }) {
 
     const components = {
         block: {
-            normal: ({children}) => <p className="leading-9 pt-4">{children}</p>,
+            normal: ({children}) => <p className="leading-9 px-4 py-2 text-base">{children}</p>,
         },
     }
 
     return (
-        <section className='px-2 md:px-0'>
+        <section className='mx-2 md:px-0 overflow-x-hidden'>
             <div className='w-full flex relative justify-center playfair-display-400 py-24 text-xl'>
-                <div className='relative md:w-1/2'>
-                    <div className='absolute p-6 bg-gradient-to-tr from-teal-800 via-sky-900 to-green-800 rounded-3xl -inset-1 z-1 blur-md' />
-                    <div className='relative p-6 bg-stone-900 rounded-3xl md:shadow-lg md:shadow-black z-10'>
-                        <h1 className='text-center playfair-display-600 text-3xl'>{project.title}</h1>
+                <div className='relative md:w-1/2 max-w-[100vw]'>
+                    <div className='absolute py-6 md:p-6 bg-gradient-to-tr from-teal-800 via-sky-900 to-green-800 rounded-3xl md:-inset-1 z-1 blur-md' />
+                    <div className='relative py-6 md:p-6 bg-stone-900 rounded-3xl md:shadow-lg md:shadow-black z-10'>
+                        <h1 className='text-center playfair-display-600 text-2xl md:text-3xl'>{project['title']}</h1>
                         <p className='text-center pb-2'>{project.publishedAt.split('T')[0]}</p>
-                        <Image src={project.imageUrl} alt={project.title} width={1920} height={1080} className='rounded-xl' />
-                        <a href={project.repoUrl} target='_blank' rel='noopener noreferrer' className='flex w-full justify-center items-center'>
+                        <div className='p-4'>
+                            <Image src={project['imageUrl']} alt={project['title']} width={1920} height={1080} className='rounded-xl' />
+                        </div>
+                        <a href={project['repoUrl']} target='_blank' rel='noopener noreferrer' className='flex w-full justify-center items-center'>
                             <Image 
                                 src={gh} 
                                 alt='Github Logo' 
@@ -37,13 +38,13 @@ export default async function Project({ params }) {
                         </a>
                         <div className='flex flex-col pt-12 blockContent'>
                             <h2 className='text-center playfair-display-600 decoration-blue-500 underline text-2xl'>Description</h2>
-                            <PortableText value={project.description} components={components} />
+                            <PortableText value={project['description']} components={components} />
                             <h2 className='text-center playfair-display-600 decoration-blue-500 underline pt-12 text-2xl'>Tech Stack</h2>
-                            <PortableText value={project.techStack} components={components} />
+                            <PortableText value={project['techStack']} components={components} />
                             <h2 className='text-center playfair-display-600 decoration-blue-500 underline pt-12 text-2xl'>Product</h2>
-                            <PortableText value={project.product} components={components} />
+                            <PortableText value={project['product']} components={components} />
                             <h2 className='text-center playfair-display-600 decoration-blue-500 underline pt-12 text-2xl'>Outcome</h2>
-                            <PortableText value={project.outcome} components={components} />
+                            <PortableText value={project['outcome']} components={components} />
                         </div>
                     </div>
                 </div>
