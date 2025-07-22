d79a5cfbbc27128784b9a73e2c417d2487c482c7
Ashton George <ashtongeorge17@gmail.com>
2025-07-22 18:59:38 -0400

    improved SEO for project pages

--- src/app/projects/[project]/page.js
+++ src/app/projects/[project]/page.js
@@ -3,7 +3,19 @@ import Link from 'next/link';
 import { client } from '@/sanity/lib/client';
 import { PROJECT_QUERY, PROJECTS_QUERY } from "@/sanity/lib/queries";
 import { PortableText } from '@portabletext/react';
-import gh from '../../../public/gh.png';
+import gh from '../../../../public/gh.png';
+
+export async function generateMetadata({ params, searchParams }, parent) {
+    const paramsRes = await params;
+    const { project: slug } = paramsRes;
+    const project = await client.fetch(PROJECT_QUERY, { slug });
+ 
+    return {
+        title: "Ashton George - " + project['title'],
+        description: project['description'],
+        images: [project['imageUrl']]
+    }
+}
 
 export default async function Project({ params }) {
     const paramsRes = await params;
@@ -20,12 +32,12 @@ export default async function Project({ params }) {
     }
 
     return (
-        <section className='mx-2 md:px-0 overflow-x-hidden flex flex-col gap-12 py-24'>
-            <div className='w-full flex relative justify-center playfair-display-400  text-xl'>
-                <div className='relative md:w-1/2 max-w-[100vw]'>
-                    <div className='absolute py-6 md:p-6 bg-gradient-to-tr from-teal-800 via-sky-900 to-green-800 rounded-3xl md:-inset-1 z-1 blur-md' />
+        <div className='mx-2 md:px-0 overflow-x-hidden flex flex-col gap-12 py-24'>
+            <section className='w-full flex relative justify-center playfair-display-400 text-xl'>
+                <div className='relative md:p-4 lg:w-1/2 max-w-screen'>
+                    <div className='absolute py-6 md:p-8 bg-gradient-to-tr from-teal-800 via-sky-900 to-green-800 rounded-3xl md:-inset-1 z-1 blur-md' />
                     <div className='relative py-6 md:p-6 bg-stone-900 rounded-3xl md:shadow-lg md:shadow-black z-10'>
-                        <h1 className='text-center playfair-display-600 text-2xl md:text-3xl'>{project['title']}</h1>
+                        <h1 className='text-center playfair-display-600 text-2xl md:text-3xl lg:text-5xl'>{project['title']}</h1>
                         <p className='text-center pb-2'>{project.publishedAt.split('T')[0]}</p>
                         <div className='p-4'>
                             <Image src={project['imageUrl']} alt={project['title']} width={1920} height={1080} className='rounded-xl' />
@@ -52,10 +64,10 @@ export default async function Project({ params }) {
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
             {otherProjects.length > 0 &&
-              <div className='w-4/5 mx-auto'>
-                <h1 className="text-2xl md:text-4xl font-semibold playfair-display-600 pb-8 text-left">My other projects</h1>
+              <section className='w-4/5 mx-auto'>
+                <h2 className="text-3xl md:text-4xl lg:text-5xl font-semibold playfair-display-600 pb-8 text-center md:text-left">My other projects</h2>
                 <div className="flex flex-col md:flex-row justify-between gap-6 h-full">
                   {[...otherProjects.slice(-3)].map((p) => 
                       <Link key={p._id} className="md:max-w-1/3" href={`/projects/${p.slug.current}`}>
@@ -66,12 +78,12 @@ export default async function Project({ params }) {
                               height={1080}
                               className='rounded-xl my-auto object-cover w-full h-full'
                           />
-                          <h1 className='text-2xl text-center playfair-display-400'>{p.title}</h1>
+                          <h3 className='text-2xl text-center playfair-display-400'>{p.title}</h3>
                       </Link>
                   )}
                 </div>
-              </div>
+              </section>
             }
-        </section>
+        </div>
     )
 }
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -3,13 +3,23 @@ import {PROJECTS_QUERY} from '@/sanity/lib/queries';
 import Image from 'next/image';
 import Link from 'next/link';
 
+export const metadata = {
+  title: "Ashton George - My Projects",
+  description: "Projects of Ashton George - Full Stack Developer and Creator.",
+  images: ["https://www.ashtonegeorge.com/og-image.png"]
+};
+
 export default async function Projects() {
     const timestamp = new Date().getTime();
     const projects = await client.fetch(PROJECTS_QUERY, { t: timestamp });
 
     return(
-        <div className="flex justify-center w-full playfair-display-600 min-h-[100vh] mt-12 md:pt-0">
-            <div className="md:w-2/3 md:h-1/2 p-12 grid lg:grid-cols-2 gap-4">
+        <div className="w-full flex flex-col align-middle items-center  min-screen mt-12 md:pt-0">
+            <section className='flex flex-col pb-8 w-full md:w-2/3 text-left gap-4 pt-12'>
+                <h1 className="text-2xl md:text-4xl xl:text-6xl font-semibold playfair-display-600">My Projects</h1>
+                <h2 className="text-xl md:text-2xl xl:text-3xl text-stone-300">Browse my previous works and learn more about what piques your interest.</h2>
+            </section>
+            <section className="md:w-2/3 md:h-1/2 p-12 grid lg:grid-cols-2 gap-4 playfair-display-600">
                 {projects.map((project) => (
                     <Link key={project._id} className="flex flex-col size-fit" href={`/projects/${project.slug.current}`}>
                         <Image
@@ -19,10 +29,10 @@ export default async function Projects() {
                             height={1080}
                             className='rounded-xl my-auto object-cover w-full h-full'
                         />
-                        <h1 className='text-2xl text-center'>{project.title}</h1>
+                        <h2 className='text-2xl text-center'>{project.title}</h2>
                     </Link>
                 ))}
-            </div>
+            </section>
         </div>
     )
 }
