32afe6f07b8a082d561a43d0090c3d0f8b410332
Ashton George <ashtongeorge17@gmail.com>
2025-07-25 23:03:43 -0400

    add hover effect, make font styles consistent

--- src/app/(public)/blog/page.js
+++ src/app/(public)/blog/page.js
@@ -13,15 +13,15 @@ export default async function BlogPage() {
     const posts = (await client.fetch(POSTS_QUERY)).reverse();
 
     return (
-        <div className="w-full flex flex-col align-middle items-center min-screen mt-12 md:pt-0">
+        <div className="w-full flex flex-col align-middle items-center min-screen my-12 md:pt-0 px-6">
             <section className='flex flex-col pb-8 w-full md:w-2/3 text-left gap-4 pt-12'>
                 <h1 className="text-2xl md:text-4xl xl:text-6xl font-semibold playfair-display-600">My Posts</h1>
-                <h2 className="text-xl md:text-2xl xl:text-3xl text-stone-300">Browse my written works and learn more about what piques your interest.</h2>
+                <h2 className="text-lg md:text-2xl xl:text-3xl text-stone-300">Browse my written works and learn more about what piques your interest.</h2>
             </section>
-            <section className="md:w-2/3 md:h-1/2 p-12 grid lg:grid-cols-3 gap-12 playfair-display-600">
+            <section className="max-w-[400px] md:max-w-max mx-auto md:w-2/3 md:h-1/2 md:p-12 grid md:grid-cols-2 xl:grid-cols-3 gap-12 playfair-display-600">
                 {posts.map((post) => (
                     <div className="relative" key={post._id}>
-                        <Link className="flex flex-col size-fit" href={`/blog/${post.slug.current}`}>
+                        <Link className="relative flex flex-col p-4 md:p-0 hover:text-sky-200 transition-colors" href={`/blog/${post.slug.current}`}>
                             <div className="relative">
                                 <div className='absolute bg-gradient-to-tr from-teal-800 via-sky-900 to-green-800 rounded-3xl md:-inset-1 z-0 blur-sm' />
                                 <Image
@@ -31,10 +31,11 @@ export default async function BlogPage() {
                                     height={1080}
                                     className='rounded-xl relative my-auto object-cover w-full h-full border border-stone-800'
                                 />
+                                <div className="absolute hidden md:block bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden rounded-xl bg-blue-400 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30" />
                             </div>
-                            <div className="text-left font-sans p-2">
-                                <h2 className='text-2xl text-left'>{post.title}</h2>
-                                <p className='text-md'>{post.publishedAt.split('T')[0]}</p>
+                            <div className="text-left p-2">
+                                <h2 className='text-xl text-left'>{post.title}</h2>
+                                <p className='text-md font-sans'>{post.publishedAt.split('T')[0]}</p>
                             </div>
                         </Link>
                     </div>
--- src/app/(public)/projects/page.js
+++ src/app/(public)/projects/page.js
@@ -14,22 +14,29 @@ export default async function Projects() {
     const projects = await client.fetch(PROJECTS_QUERY, { t: timestamp });
 
     return(
-        <div className="w-full flex flex-col align-middle items-center  min-screen mt-12 md:pt-0">
+        <div className="w-full flex flex-col align-middle items-center min-screen my-12 md:pt-0 px-6">
             <section className='flex flex-col pb-8 w-full md:w-2/3 text-left gap-4 pt-12'>
                 <h1 className="text-2xl md:text-4xl xl:text-6xl font-semibold playfair-display-600">My Projects</h1>
-                <h2 className="text-xl md:text-2xl xl:text-3xl text-stone-300">Browse my previous works and learn more about what piques your interest.</h2>
+                <h2 className="text-lg md:text-2xl xl:text-3xl text-stone-300">Browse my previous works and learn more about what piques your interest.</h2>
             </section>
-            <section className="md:w-2/3 md:h-1/2 p-12 grid lg:grid-cols-2 gap-4 playfair-display-600">
+            <section className="md:w-2/3 md:h-1/2 md:p-12 grid lg:grid-cols-2 gap-4 playfair-display-600">
                 {projects.map((project) => (
-                    <Link key={project._id} className="flex flex-col size-fit" href={`/projects/${project.slug.current}`}>
-                        <Image
-                            src={project.imageUrl} 
-                            alt={project.title ? project.title : 'Project Image'}
-                            width={1920}
-                            height={1080}
-                            className='rounded-xl my-auto object-cover w-full h-full'
-                        />
-                        <h2 className='text-2xl text-center'>{project.title}</h2>
+                    <Link key={project._id} className="relative flex flex-col p-4 md:p-0 hover:text-sky-200 transition-colors" href={`/projects/${project.slug.current}`}>
+                        <div className='relative'>
+                            <div className='absolute bg-gradient-to-tr from-teal-800 via-sky-900 to-green-800 rounded-3xl md:-inset-1 z-0 blur-sm' />
+                            <Image
+                                src={project.imageUrl} 
+                                alt={project.title ? project.title : 'Project Image'}
+                                width={1920}
+                                height={1080}
+                                className='relative rounded-xl my-auto aspect-video w-full min-h-24 xl:min-h-48 2xl:min-h-[12vw] hover:brightness-110 transition-all duration-300'
+                            />
+                            <div className="absolute hidden md:block bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden rounded-xl bg-blue-400 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30" />
+                        </div>
+                        <div className='pl-3 pt-2'>
+                            <h3 className='text-xl text-left'><strong>{project.title}</strong></h3>
+                            <p className='text-md text-left font-sans'>{project.publishedAt.split('T')[0]}</p>
+                        </div>
                     </Link>
                 ))}
             </section>
